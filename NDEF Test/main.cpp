9262dd1799c584b87402df68999c038fb948afe5
Timothy Roe <timothyroejr@timothys-mbp.home>
2017-07-30 10:50:49 -0700

    Initial Commit

---
+++ NDEF Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NDEF Test
+//
+//  Created by Timothy Roe Jr. on 7/30/17.
+//  Copyright © 2017 Timothy Roe Jr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
